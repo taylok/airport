6604753ed2190a65bc0c5a3c8423c10d751906bf
Keith <taylor.km61@icloud.com>
2020-08-21 13:01:19 +0100

    Refactor from procedural to polymorphism. Flight should not contain type switch. Instead have one separate flight class for each conditional type: economy, business. Flight then becomes abstract class.

--- src/main/java/com/pluralsight/tddjunit5/airport/Flight.java
+++ src/main/java/com/pluralsight/tddjunit5/airport/Flight.java
@@ -4,15 +4,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Flight {
+public abstract class Flight {
 
 	private String id;
-	private List<Passenger> passengersList = new ArrayList<Passenger>();
-	private String flightType;
+	List<Passenger> passengersList = new ArrayList<Passenger>();
 
-	public Flight(String id, String flightType) {
+	public Flight(String id) {
 		this.id = id;
-		this.flightType = flightType;
 	}
 
 	public String getId() {
@@ -23,37 +21,7 @@ public class Flight {
 		return Collections.unmodifiableList(passengersList);
 	}
 
-	public String getFlightType() {
-		return flightType;
-	}
+	abstract boolean addPassenger( Passenger passenger );
 
-	public boolean addPassenger(Passenger passenger) {
-		switch (flightType) {
-			case "Economy":
-				return passengersList.add(passenger);
-			case "Business":
-				if (passenger.isVip()) {
-					return passengersList.add(passenger);
-				}
-				return false;
-			default:
-				throw new RuntimeException("Unknown type: " + flightType);
-		}
-
-	}
-
-	public boolean removePassenger(Passenger passenger) {
-		switch (flightType) {
-			case "Economy":
-				if (!passenger.isVip()) {
-					return passengersList.remove(passenger);
-				}
-				return false;
-			case "Business":
-				return false;
-			default:
-				throw new RuntimeException("Unknown type: " + flightType);
-			}
-	}
-
+	abstract boolean removePassenger( Passenger passenger );
 }
