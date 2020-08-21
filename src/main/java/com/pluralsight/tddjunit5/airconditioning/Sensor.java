1acd4748a55693843dc672e8b1a10ade4a3dbb1c
Keith <taylor.km61@icloud.com>
2020-08-21 15:11:01 +0100

    First Air Con Implemntation.  Test ... fail

---
+++ src/main/java/com/pluralsight/tddjunit5/airconditioning/Sensor.java
@@ -0,0 +1,15 @@
+package com.pluralsight.tddjunit5.airconditioning;
+
+public class Sensor {
+
+    private boolean blocked;
+
+    public boolean isBlocked() {
+        return blocked;
+    }
+
+    public void setBlocked(boolean blocked) {
+        this.blocked = blocked;
+    }
+
+}
---
+++ src/main/java/com/pluralsight/tddjunit5/airconditioning/Thermometer.java
@@ -0,0 +1,23 @@
+package com.pluralsight.tddjunit5.airconditioning;
+
+public class Thermometer {
+
+    private double temperature;
+    private Sensor sensor;
+
+    public double getTemperature() {
+        return 0;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public Sensor getSensor() {
+        return sensor;
+    }
+
+    public void setSensor(Sensor sensor) {
+        this.sensor = sensor;
+    }
+}
