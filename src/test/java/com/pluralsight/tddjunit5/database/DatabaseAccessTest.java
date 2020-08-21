69d9d2a67bd9350c0203831f2b7aa0c850ee7093
Keith <taylor.km61@icloud.com>
2020-08-21 15:37:17 +0100

    Test database implementation with mock.  Additional Test

--- src/test/java/com/pluralsight/tddjunit5/database/DatabaseAccessTest.java
+++ src/test/java/com/pluralsight/tddjunit5/database/DatabaseAccessTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -20,5 +20,17 @@ public class DatabaseAccessTest {
         when(database.login(credentials)).thenReturn(true);
         Credentials sameCredentials = new Credentials("user", "password");
         assertTrue(database.login(sameCredentials));
+    }
+
+    @Test
+    void testUserFailedLogin() {
+        when(database.login(credentials)).thenReturn(true);
+        Credentials otherCredentials = new Credentials("user", "password");
+        otherCredentials.setUsername("otherUser");
+        otherCredentials.setPassword("otherPassword");
+        assertNotEquals(credentials.getUsername(), otherCredentials.getUsername());
+        assertNotEquals(credentials.getPassword(), otherCredentials.getPassword());
+        assertNotEquals(credentials.hashCode(), otherCredentials.hashCode());
+        assertFalse(database.login(otherCredentials));
     }
 }
